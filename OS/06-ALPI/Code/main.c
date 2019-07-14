342f24ef842ec6e740a15cc66807e5573f512245
Ztiany <Ztiany3@gmail.com>
2019-07-14 23:16:22 +0800

    update

--- OS/06-ALPI/Code/main.c
+++ OS/06-ALPI/Code/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include "get_num.h"
 
 int main() {
     printf("Hello, World!\n");
+
+    char *str = "123";
+
+    int result = getInt(str, 0, "");
+
+    printf("%d", result);
+
     return 0;
 }

d2f7038a0c0139f7be61e6d603301ba195f9ead9
Ztiany <Ztiany3@gmail.com>
2019-08-06 00:06:11 +0800

    update

---
+++ OS/06-TLPI/chapter05/example/01_bad_exclusive_open.c
@@ -0,0 +1,17 @@
+/*
+ =========================================================
+ Author       ：ztiany 
+ Date         ：19-8-6
+ Description  ：以独占的方式打开文件（错误方式）
+ =========================================================
+ */
+
+
+#include <fcntl.h>
+#include <sys/stat.h>
+#include "tlpi_hdr.h"
+
+
+int main(int argc, char *argv[]) {
+    return EXIT_SUCCESS;
+}
